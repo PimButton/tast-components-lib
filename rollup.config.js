2f89d50c6b6a5038655a5d2647620e0655258685
Roland Branten <roland.branten@gmail.com>
2022-09-28 16:53:48 +0200

    Optimize rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 const packageJson = require("./package.json");
 
 export default [
@@ -22,10 +23,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      terser(),
     ],
   },
   {
