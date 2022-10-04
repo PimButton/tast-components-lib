b0cd060c015387639d17c5cd14c2e6e2201ab9c2
Roland Branten <roland.branten@gmail.com>
2022-10-04 14:03:49 +0200

    remove lang context export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { Button } from "./Button/Button";
 export { Input } from "./Input/Input";
 export { FormContext, FormContextWrapper } from "./utils/formContext";
-export { Layout, LangContext } from "./Layout/Layout";
+export { Layout } from "./Layout/Layout";
 export { useFormValues } from "./utils/useFormValues";
